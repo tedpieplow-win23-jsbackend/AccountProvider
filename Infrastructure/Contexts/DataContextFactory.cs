08299ab33842424c6bb036ade3e7466e2fe77742
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-05-07 19:16:48 +0200

    removed visible connection string.

--- Infrastructure/Contexts/DataContextFactory.cs
+++ Infrastructure/Contexts/DataContextFactory.cs
@@ -8,7 +8,7 @@ public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-thjr.database.windows.net,1433;Initial Catalog=Silicon-AccountProvider-Db;Persist Security Info=False;User ID=SQLadmin;Password=Fillja123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("");
 
         return new DataContext(optionsBuilder.Options);
     }
